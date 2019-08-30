98bf9efea9ee863d9488a8a4417bf6b0d7ff6751
thiagoluiznunes <thiagoluiz.dev@gmail.com>
2019-08-30 20:46:56 -0300

    create connection with firebase

---
+++ config/firebase.js
@@ -0,0 +1,15 @@
+// import fs from 'fs';
+import path from 'path';
+import * as admin from 'firebase-admin';
+
+const serviceAccount = path.join(__dirname, '../serviceAccountKey.json');
+const initConnection = () => {
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: 'https://auth-service-50009.firebaseio.com/'
+  });
+}
+
+export default {
+  initConnection,
+}
