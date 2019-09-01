ae3f9726fc194ce7dbfb390deea3100a0ec14ff7
thiagoluiznunes <thiagoluiz.dev@gmail.com>
2019-09-01 01:50:52 -0300

    update build and prod scripts

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,4 +4,5 @@ rm -rf ./dist
 ./node_modules/.bin/babel ./components --out-dir dist/components
 ./node_modules/.bin/babel ./config --out-dir dist/config
 ./node_modules/.bin/babel ./app.js --out-dir dist
+cp ./serviceAccountKey.json dist
 
